3db3eb48bc15a23eab9bdc80881452ed5f8d4779
DylanMorison <dylan@g.morison.org>
2022-11-25 16:47:23 -0800

    fix

--- server/cmd/api/utils.go
+++ server/cmd/api/utils.go
@@ -59,7 +59,7 @@ func (app *application) readJson(w http.ResponseWriter, r *http.Request, data in
 func (app *application) errorJson(w http.ResponseWriter, err error, status ...int) error {
 	statusCode := http.StatusBadRequest
 
-	if len(status > 0) {
+	if len(status) > 0 {
 		statusCode = status[0]
 	}
 
